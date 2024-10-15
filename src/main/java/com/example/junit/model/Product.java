bc75617c594a12a7f26dbf334ba15feacc68e6f6
ROHITH PARIMELLA <87310735+rohithdeveloper@users.noreply.github.com>
2024-10-15 23:31:01 +0530

    Update Product.java

--- src/main/java/com/example/junit/model/Product.java
+++ src/main/java/com/example/junit/model/Product.java
@@ -22,7 +22,7 @@ import lombok.NoArgsConstructor;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "productId")
     private Long id;
     
